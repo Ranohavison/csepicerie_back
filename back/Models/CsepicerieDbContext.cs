e074456311d05f5a94d5204f3e8d4477babee6c2
Ranohavison <ranohavison399@gmail.com>
2025-07-21 23:45:34 +0300

    Pret pour le deploiement

--- back/Models/CsepicerieDbContext.cs
+++ back/Models/CsepicerieDbContext.cs
@@ -52,8 +52,21 @@ public partial class CsepicerieDbContext : DbContext
     public virtual DbSet<Vente> Ventes { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=csepicerie_db;Username=csepicerie_user;Password=csEpiceriePass");
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseNpgsql(connectionString);
+        }
+    }
+// #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        // => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=csepicerie_db;Username=csepicerie_user;Password=csEpiceriePass");
+        // => optionsBuilder.UseNpgsql("postgresql://neondb_owner:npg_n9fVOmLl4TPQ@ep-small-recipe-ab8ufpbg-pooler.eu-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require;Trust Server Certificate=true");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
